dc9e1eb9beffd6c6d67355fa668df75e5d47897b
joomla <joomla@10.2.222.228>
2014-10-16 09:48:38 +0200

    kjhkjkjh

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-10-15 13:46:43 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-10-15	13:46:43	INFO	127.0.0.1	Joomla FAILURE: 	Nome utente e password non corretti o non hai ancora un account.
